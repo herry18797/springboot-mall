4cad52696e94d1ca6d6e97f781227d710451d28d
team00005 <openfome18797@gmail.com>
2023-03-17 13:18:34 +0800

    商品功能-Refactor category，使用Eunm類型

---
+++ src/main/java/com/herry/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.herry.springbootmall.constant;
+
+public enum ProductCategory {
+    FOOD,
+    CAT,
+    E_BOOK
+}
--- src/main/java/com/herry/springbootmall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/herry/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.herry.springbootmall.rowmapper;
 
+import com.herry.springbootmall.constant.ProductCategory;
 import com.herry.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,7 +15,14 @@ public class ProductRowMapper implements RowMapper<Product> {
         Product product=new Product();
         product.setProduct_id(resultSet.getInt("product_id"));
         product.setProduct_name(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+
+
+        String categoryStr=resultSet.getString("category");
+        ProductCategory category=ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
+        //product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
+
         product.setImage_url(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
